f3cd3052080eb899cf0107e90b28505801564d13
Mohammad-shehab <q8mlg@outlook.com>
2024-12-17 11:58:20 +0300

    update

--- bmi/Program.cs
+++ bmi/Program.cs
@@ -5,62 +5,60 @@ double height_cm = Convert.ToDouble(Console.ReadLine());
 double height_m = height_cm / 100;
 double bmi = (weight /  Math.Pow(height_m,2));
 Console.WriteLine("Your BMI is: " + bmi );
+string BMIState="";
 if  (bmi <18.5)
 {
-    Console.WriteLine("you are underweight");
+    BMIState = "underweight";
 }
 else if (bmi >= 18.5 && bmi <= 24.9)
 {
-    Console.WriteLine("you are healthy");
+    BMIState = "healthy";
 }
 else if (bmi >= 25.0 && bmi <= 29.9)
 {
-    Console.WriteLine("you are overweight");
+    BMIState = "overweight";
 }
 else if (bmi >= 30)
 {
-    Console.WriteLine("you are obese");
+    BMIState = "obese";
 }
 else
 {
     Console.WriteLine("Error");
 }
-
-
-string BMIState = Convert.ToString(bmi);
+Console.WriteLine("You are " + BMIState);
 
 Console.WriteLine("---------------------------------------------------------");
 Console.WriteLine("Enter your Age: ");
 double age = Convert.ToDouble(Console.ReadLine());
-
+string BMIState2 = "";
 
 if (bmi >= 19 && bmi <= 24 && age >=19 && age<=24 )
 {
-    Console.WriteLine("You are Healthy");
+    BMIState2 = "Healthy";
 }
 else if (bmi >= 20 && bmi <= 25 && age >= 25 && age <= 34)
 {
-    Console.WriteLine("You are Healthy");
+    BMIState2 = "Healthy";
 }
 else if (bmi >= 21 && bmi <= 26 && age >= 35 && age <= 44)
 {
-    Console.WriteLine("You are Healthy");
+    BMIState2 = "Healthy";
 }
 else if (bmi >= 22 && bmi <= 27 && age >= 45 && age <= 54)
 {
-    Console.WriteLine("You are Healthy");
+    BMIState2 = "Healthy";
 }
 else if (bmi >= 23 && bmi <= 28 && age >= 55 && age <= 64)
 {
-    Console.WriteLine("You are Healthy");
+    BMIState2 = "Healthy";
 }
 else if (bmi >= 24 && bmi <= 29 && age >= 65)
 {
-    Console.WriteLine("You are Healthy");
-    
+    BMIState2 = "Healthy";
 }
 else
 {
-    Console.WriteLine("You are not Healthy");
+    BMIState2 = " Not Healthy";
 }
-string BMIState2 = Convert.ToString(bmi);
+Console.WriteLine("You are " + BMIState2);
